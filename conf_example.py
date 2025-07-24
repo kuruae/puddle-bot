7064f01ff7aa2498f986b513e0b4d626f6f718e2
Enzo <kurutsukipv@gmail.com>
2025-07-24 03:32:19 +0200

    fixed readme since there is no config.py anymore

--- conf_example.py
+++ conf_example.py
@@ -4,6 +4,6 @@
 DISCORD_TOKEN = "YOUR_DISCORD_BOT_TOKEN"
 CHANNEL_ID = 00000000000000  # Replace with your channel ID
 PLAYER_IDS = {
-    "player1": "0000000000000000",
+    "player1": "0000000000000000", # Replace with actual player IDs which can be found in the Puddle Farm API
     "player2": "11111111111111111"
 }

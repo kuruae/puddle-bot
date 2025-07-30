bbcc0eaddb764b81ff120e5ad8caa863963f3ff0
kuruae <kurutsuipv@gmail.com>
2025-07-30 20:26:15 +0200

    pylint fonctionne en local stp sale enculeeeeeee

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ except ImportError as exc:
 POLL_INTERVAL = int(os.getenv('POLL_INTERVAL', '2'))  # 2 minutes default
 
 
-class GGSTBot(commands.Bot(owner_id=int(os.getenv('BOT_OWNER_ID')))):
+class GGSTBot(commands.Bot):
     """Main GGST Match Tracker Bot"""
 
     def __init__(self):
